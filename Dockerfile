739925f655d7a10e52eaf3f3c8e7fdf1f1b4e56b
Christian Kniep <christian@qnib.org>
2017-05-09 13:49:40 +0200

    refine send-tcp-event.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM qnib/alplain-init
 
-ENV SKIP_ENTRYPOINTS=1
+ENV SKIP_ENTRYPOINTS=1 \
+    HOST=172.17.0.2 \
+    PORT=11001
 RUN apk --no-cache add netcat-openbsd
 COPY bin/send-tcp-event.sh /usr/local/bin/
--- bin/send-tcp-event.sh
+++ bin/send-tcp-event.sh
@@ -3,8 +3,8 @@
 HOST=${HOST:-172.17.0.2}
 PORT=${PORT:-11001}
 EVENT_CODE=${1:-001.001}
-DATA=${2}
-if [[ "X${DATA}" == "X" ]];then
-  DATA=$(date)
+MSG=$2
+if [[ "X${MSG}" == "X" ]];then
+  MSG="My message is $(date)"
 fi
-echo "cee{\"data\":\"${DATA}\", \"event_code\":\"${EVENT_CODE}\"}" |nc ${HOST} ${PORT}
+echo "cee{\"msg\":\"${MSG}\", \"event_code\":\"${EVENT_CODE}\"}" |nc ${HOST} ${PORT}
