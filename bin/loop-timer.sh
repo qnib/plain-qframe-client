5e59c4243abf6150452ea2ad9a3d386443b8ab7c
Christian Kniep <christian@qnib.org>
2017-05-18 13:07:38 +0200

    improve timer-loop

--- bin/loop-timer.sh
+++ bin/loop-timer.sh
@@ -5,8 +5,9 @@ trap "exit" SIGTERM SIGKILL
 DELAY=${DELAY:-2}
 CNT=${1:-5}
 NAME=${2:-testTimer}
+MIN=${MIN:-10000}
 for x in $(seq 1 ${CNT});do
-   MSG="${NAME}:${RANDOM}|ms"
+   MSG="${NAME}:$((${RANDOM}%${MIN}+${MIN}))|ms"
    send-tcp-msg.sh ${MSG}
    sleep ${DELAY}
 done
