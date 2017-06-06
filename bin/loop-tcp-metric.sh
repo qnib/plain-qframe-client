25d581558fcb588a2bb38023e560d4b2a2af1bd3
Christian Kniep <christian@qnib.org>
2017-06-06 09:00:24 +0200

    allow configurable DELAY

--- bin/loop-tcp-metric.sh
+++ bin/loop-tcp-metric.sh
@@ -4,10 +4,11 @@ HOST=${HOST:-172.17.0.2}
 PORT=${PORT:-11001}
 LOOP=${1:-1}
 NAME=${2:-testmetric}
+DELAY=${DELAY:-2}
 
 for x in $(seq 1 ${LOOP});do 
   msg="put ${NAME} $(date +%s) ${RANDOM} ${3}"
   echo "Send: $msg"
   echo $msg |nc ${HOST} ${PORT}
-  sleep 2
+  sleep ${DELAY}
 done
