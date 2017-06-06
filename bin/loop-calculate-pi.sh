64e272efd6c5e006e6af8da2397b2f25e0f8b48b
Christian Kniep <christian@qnib.org>
2017-06-06 15:08:43 +0200

    fix bin/loop-calculate-pi.sh

--- bin/loop-calculate-pi.sh
+++ bin/loop-calculate-pi.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 DELAY=${DELAY:-2}
+LOOP=${1:--1}
 CNT=${1:-5}
 MSG=$2
-for x in $(seq 1 ${CNT});do
-   curl ${HOST}:8080/pi/$(((${RANDOM}%9999)*10))
-   sleep ${DELAY}
+trap exit INT
+
+while [[ ${LOOP} != 0 ]]; do
+  curl -s ${HOST}:8080/pi/$(((${RANDOM}%9999)*10))
+  sleep ${DELAY}
+  if [[ ${LOOP} > 0 ]];then
+    LOOP=$(echo $((${LOOP}-1)))
+  fi
 done
