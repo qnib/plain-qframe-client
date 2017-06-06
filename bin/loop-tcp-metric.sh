fe1334d0eee05ce57e5fe0549500666f4685d34d
Christian Kniep <christian@qnib.org>
2017-06-06 09:44:36 +0200

    add MIN limitation to loop-tcp-metrics.sh

--- bin/loop-tcp-metric.sh
+++ bin/loop-tcp-metric.sh
@@ -5,11 +5,12 @@ PORT=${PORT:-11001}
 LOOP=${1:--1}
 NAME=${2:-testmetric}
 DELAY=${DELAY:-2}
+MIN=${MIN:-1000}
 
 trap exit INT
 
 while [[ ${LOOP} != 0 ]]; do
-  msg="put ${NAME} $(date +%s) ${RANDOM} ${3}"
+  msg="put ${NAME} $(date +%s) $((${RANDOM}%${MIN}+${MIN})) ${3}"
   echo "Send: $msg"
   echo $msg |nc ${HOST} ${PORT}
   sleep ${DELAY}
