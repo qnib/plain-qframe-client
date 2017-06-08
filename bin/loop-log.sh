3c83de717d4fd4a057956f1bfb7184e36f57276b
Christian Kniep <christian@qnib.org>
2017-06-08 12:41:19 +0200

    enhance loop-log with endless loop

--- bin/loop-log.sh
+++ bin/loop-log.sh
@@ -3,9 +3,19 @@
 trap exit INT
 
 DELAY=${DELAY:-2}
-LOG_LEVEL=${2:-INFO}
-CNT=${1:-5}
-for x in $(seq 1 ${CNT});do
-   echo "[${LOG_LEVEL}] Log message No${x} at unixepoch $(date +%s)"
-   sleep ${DELAY}
+LOG_LEVEL=${1:-INFO}
+LOOP=${2:--1}
+trap exit INT
+
+cnt=1
+while [[ true ]]; do
+  echo "[${LOG_LEVEL}] Log message No${cnt} at unixepoch $(date +%s)"
+  if [[ ${LOOP} > 0 ]];then
+    LOOP=$(echo $((${LOOP}-1)))
+    if [[ ${LOOP} == 0 ]];then
+      break
+    fi
+  fi
+  sleep ${DELAY}
+  cnt=$((${cnt}+1))
 done
